0e31a5bbdc881a3bd03add817ab79e0b8945213d
Maksym Demeshok <maksymdemeshok@MacBook-Pro-Maksym.local>
2021-11-21 18:54:55 +0100

    Adding md5 hashing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 
 const app = express();
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+
 
 
 const User = new mongoose.model('User', userSchema);
@@ -48,7 +48,7 @@ app.post('/register', (req, res) => {
 
     let newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(err => {
         if (!err) {
@@ -63,7 +63,7 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne(
         { email: username },
@@ -71,7 +71,7 @@ app.post('/login', (req, res) => {
             if (!err) {
                 if (foundUser) {
                     if (foundUser.password === password) {
-                        
+                        console.log(password)
                         res.render('secrets');
                     } else {
                         console.log('Wrong password');
